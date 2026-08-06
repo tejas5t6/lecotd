67417f228dafc0781336d5293975f87fb04a4ccd
tejas5t6 <pandeytejas08@gmail.com>
2026-08-06 19:31:08 +0530

    Time: 0 ms (100%), Space: 42.2 MB (41.54%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int m = x^y;
+        int count=0;
+        while(m>0){
+            count++;
+            m=m&(m-1);
+        }
+        return count;
+    }
+}
