7308f0d68ab6838e8c548a39553bf5cb6bcb32db
tejas5t6 <pandeytejas08@gmail.com>
2026-08-23 12:27:50 +0530

    Time: 5 ms (28.67%), Space: 51.5 MB (82.99%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,64 @@
+class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
+    public void bfs(int i,int j,char[][]grid,boolean[][]visited){
+         int m=grid.length;
+        int n=grid[0].length;
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(i,j));
+        while(q.size()>0){
+            Pair frnt =q.remove();
+            int row=frnt.row;
+            int col=frnt.col;//we dont need to traverse for top and left side of the array
+            //top-> row-1,col
+            if(row>0){
+                if(visited[row-1][col]==false && grid[row-1][col]=='1'){
+                    q.add(new Pair(row-1,col));
+                    visited[row-1][col]=true;
+                }
+            }
+            //bottom-> row+1,col
+             if(row+1<m){
+                if(visited[row+1][col]==false && grid[row+1][col]=='1'){
+                    q.add(new Pair(row+1,col));
+                    visited[row+1][col]=true;
+                }
+             }
+            //  left->row,col-1
+              if(col>0){
+                if(visited[row][col-1]==false && grid[row][col-1]=='1'){
+                    q.add(new Pair(row,col-1));
+                    visited[row][col-1]=true;
+                }
+              }
+            //   right-> row,col+1
+               if(col+1<n){
+                if(visited[row][col+1]==false && grid[row][col+1]=='1'){
+                    q.add(new Pair(row,col+1));
+                    visited[row][col+1]=true;
+                }
+               }
+        }
+    }
+    public int numIslands(char[][] grid) {
+        boolean[][]visited=new boolean[grid.length][grid[0].length];
+        int m=grid.length;
+        int n=grid[0].length;
+        int cnt=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && visited[i][j]==false){
+                    bfs(i,j,grid,visited);
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
