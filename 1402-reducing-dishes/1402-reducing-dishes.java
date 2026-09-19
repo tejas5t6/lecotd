1e8a046158179dda87f5695ad35f9c5fd12dbf80
tejas5t6 <pandeytejas08@gmail.com>
2026-09-19 20:23:58 +0530

    Time: 20 ms (15.1%), Space: 48.2 MB (17.65%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -0,0 +1,22 @@
+class Solution {
+    int[][] dp;
+    private int solve(int[] s, int i, int t,int n) {
+        if (i == n) {
+            return 0;
+        }
+        if (dp[i][t]!=-1) return dp[i][t];
+        int take = s[i] * t + solve(s, i + 1, t + 1,n);
+        int skip = solve(s, i + 1, t,n);
+        return dp[i][t] = Math.max(take,skip);
+    }
+
+    public int maxSatisfaction(int[] s) {
+        int n = s.length;
+        Arrays.sort(s);
+        dp = new int[n+1][n+2];
+        for (int[] row:dp) {
+            Arrays.fill(row, -1);
+        }
+        return solve(s,0,1,n);
+    }
+}
